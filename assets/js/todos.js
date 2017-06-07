54d6f934b605d5c801af1e7c0520aeeb3f84d39c
Matt Palamos <mattpalamos@hotmail.com>
2017-06-06 21:56:46 -0700

    added function to add into list and remove from list by clicking x

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,25 @@
 //check off specific todos by clicking
 
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
   //if li is gray
   $(this).toggleClass('completed')
 });
 //click on x to delete todo
-$('span').click(function(event) {
+$('ul').on('click', 'span', function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove()
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    //grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val('');
+    //create new li and add to ul
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+    //takes a string of elements and append them
+  }
+  //enter is 13
 });
